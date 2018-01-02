4c0f47777fb789924760e3091b8a3f7d96ec138f
TjSanshao <1305705188@qq.com>
2018-01-02 19:02:03 +0800

    测试MVC的参数绑定

---
+++ ModelBindTest/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelBindTest.Models
+{
+    public class Person
+    {
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
