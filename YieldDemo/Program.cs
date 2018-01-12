127fa3e50518ded0de9e60b6c9041492397d1e8e
TjSanshao <1305705188@qq.com>
2018-01-13 01:38:29 +0800

    yield关键字

---
+++ YieldDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            foreach (var n in person)
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class Person : IEnumerable<string>
+    {
+
+        private string[] _names = { "tj1", "tj2", "tj3" };
+
+        public string[] Names { get => _names; set => _names = value; }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            for (int i = 0; i < Names.Length; i++)
+            {
+                yield return Names[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<string>)Names).GetEnumerator();
+        }
+    }
+}
