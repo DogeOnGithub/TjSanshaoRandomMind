6d7d1af3fade48aceca4e6288131ce500a4272b1
TjSanshao <1305705188@qq.com>
2018-01-28 01:08:09 +0800

    访问修饰符是对于类来说的，不是对于对象来说的，同类型的对象是可以互相访问私有成员的

---
+++ ClassDemo/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class Person
+    {
+        private int age;
+        private string name;
+
+        public int Age { get => age; set => age = value; }
+
+        public void Test(ref Person person)
+        {
+            Console.WriteLine(person.age);
+            person.Age = 44;
+        }
+    }
+}
---
+++ ClassDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person { Age = 22 };
+            Person person2 = new Person { Age = 33 };
+            person1.Test(ref person2);
+            Console.WriteLine(person2.Age);
+            Console.ReadKey();
+        }
+    }
+}
